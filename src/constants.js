d8c40b51155aefccb9dd2b3fefa237abff94d996
Teja <phankanmp@gmail.com>
2019-05-05 18:29:21 -0700

    Changed server urls

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
-export const SERVER_URL = 'http://localhost:8080/api/'
-export const AUTH_SERVER_URL = 'http://localhost:8080/api/auth/'
+export const SERVER_URL = "http://courses-app-backend.herokuapp.com/api/";
+export const AUTH_SERVER_URL =
+  "http://courses-app-backend.herokuapp.com/api/auth/";
